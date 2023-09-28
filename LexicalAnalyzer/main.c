4e21d65f42be74fe2c9547406d73feb00bfaec7d
aleksandrstarzhynskyi <a.stargik@gmail.com>
2023-09-28 14:58:37 +0300

    Initial Commit

---
+++ LexicalAnalyzer/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LexicalAnalyzer
+//
+//  Created by Aleksandr Starzhynskyi on 28.09.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
